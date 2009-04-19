ff77b606fa091a5e1959eb79bb42af9a75d2426c
tyler <tyler@idyle.net>
2009-04-18 18:43:24 -0700

    deleted cruft from past version

--- app/models/location.rb
+++
@@ -1,4 +0,0 @@
-class Location < ActiveRecord::Base
-  validates_presence_of     :line1, :city, :region, :zipcode, :country
-  validates_length_of       :line1, :city, :region, :country, :within => 4..40
-end
